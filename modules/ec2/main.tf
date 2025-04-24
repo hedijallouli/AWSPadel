d6330efdc993bbc0d68bd3b122da69d13f4100c8
hedijallouli <hedijallouli.93@gmail.com>
2025-04-24 20:26:41 +0200

    fix: update user_data for Amazon Linux 2023 in EC2 module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -9,16 +9,16 @@ resource "aws_instance" "wordpress" {
 
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    amazon-linux-extras install -y php7.4
-    yum install -y httpd mariadb php php-mysqlnd
-    systemctl enable httpd
-    systemctl start httpd
+    dnf update -y
+    dnf install -y httpd php php-mysqli php-fpm tar wget
+
+    systemctl enable --now httpd
+
     cd /var/www/html
+    rm -rf *
     wget https://wordpress.org/latest.tar.gz
     tar -xzf latest.tar.gz
     cp -r wordpress/* .
-    rm -rf wordpress latest.tar.gz
     chown -R apache:apache /var/www/html
     chmod -R 755 /var/www/html
   EOF
