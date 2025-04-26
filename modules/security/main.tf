275a391211bfd8a673fbd44c2446dcaa3649f0ab
hedijallouli <hedijallouli.93@gmail.com>
2025-04-26 23:42:05 +0200

    git add . git commit -m "feat(security): add Bastion security group for SSH access" git push

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -25,3 +25,29 @@ resource "aws_security_group" "ec2_sg" {
     Name = "wordpress-ec2-sg"
   }
 }
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH from trusted IP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH access from trusted IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_access_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-sg"
+  }
+}
