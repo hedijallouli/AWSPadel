90a21191d13dc13689db91b99e5892ea9a30519c
hedijallouli <hedijallouli.93@gmail.com>
2025-04-26 23:14:35 +0200

    bastion missing

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "wordpress-ec2-sg"
-  description = "Allow HTTP from ALB and optional SSH"
+  description = "Allow HTTP from ALB"
   vpc_id      = var.vpc_id
 
   # Ingress rule to allow HTTP access from anywhere (for temporary testing)
@@ -10,15 +10,6 @@ resource "aws_security_group" "ec2_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Ingress rule to allow SSH access only from the specified IP
-  ingress {
-    description = "Allow SSH from your IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_access_cidr]
   }
 
   # Egress rule to allow all outbound traffic
