6b1a73f623855705d59f1faa14f1177f7681dfe8
Hedi Jallouli <hedijallouli.93@gmail.com>
2025-05-10 19:57:51 +0100

    fix issue (autoscalling)

--- modules/autoscaling/outputs.tf
+++ modules/autoscaling/outputs.tf
@@ -2,30 +2,30 @@
 
 output "asg_name" {
   description = "The name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.wordpress_asg.name
+  value       = aws_autoscaling_group.wordpress.name
 }
 
 output "asg_arn" {
   description = "The ARN of the Auto Scaling Group"
-  value       = aws_autoscaling_group.wordpress_asg.arn
+  value       = aws_autoscaling_group.wordpress.arn
 }
 
 output "launch_template_id" {
   description = "The ID of the launch template used"
-  value       = aws_launch_template.wordpress_launch_template.id
+  value       = aws_launch_template.wordpress_launch.id
 }
 
 output "asg_min_size" {
   description = "Minimum size of the Auto Scaling Group"
-  value       = aws_autoscaling_group.wordpress_asg.min_size
+  value       = aws_autoscaling_group.wordpress.min_size
 }
 
 output "asg_max_size" {
   description = "Maximum size of the Auto Scaling Group"
-  value       = aws_autoscaling_group.wordpress_asg.max_size
+  value       = aws_autoscaling_group.wordpress.max_size
 }
 
 output "asg_desired_capacity" {
   description = "Desired capacity of the Auto Scaling Group"
-  value       = aws_autoscaling_group.wordpress_asg.desired_capacity
+  value       = aws_autoscaling_group.wordpress.desired_capacity
 }
