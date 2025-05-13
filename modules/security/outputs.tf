67fc29d03ac715c8702c81e19a4308809fd96509
Hedi Jallouli <hedijallouli.93@gmail.com>
2025-05-13 10:56:53 +0100

    fix 7

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -6,8 +6,3 @@ output "bastion_sg_id" {
   description = "Security Group ID for the Bastion host"
   value       = aws_security_group.bastion_sg.id
 }
-
-output "alb_sg_id" {
-  description = "Security Group ID for the ALB"
-  value       = aws_security_group.alb_sg.id
-}
