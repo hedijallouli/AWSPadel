6b970748c75a53f15e4392d4881c9db1c4809fd4
Hedi Jallouli <hedijallouli.93@gmail.com>
2025-05-13 19:23:46 +0100

    fix changes #2

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -7,7 +7,3 @@ output "private_ip" {
   description = "Private IP of the EC2 instance"
   value       = aws_instance.wordpress.private_ip
 }
-
-output "ec2_sg_id" {
-  value = aws_security_group.ec2_sg.id
-}
